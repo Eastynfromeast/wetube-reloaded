74e01aaef8d4c99740fd548d0cdeafdd136fd09f
Eastyn <eastynwork@gmail.com>
2024-09-02 08:13:58 +0900

    #5.0 Returning HTML - we cannot send the whole html with res.send(), it's too big -

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,12 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) =>
+	res.send(
+		"<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home Page Videos</h1><footer>&copy;2024 Wetube -  All rights reserved</footer><body></html>"
+	);
 export const see = (req, res) => {
 	console.log(req.params);
-	res.send(`Watch Video #${req.params.id}`);
+	res.send(
+		`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch Video #${req.params.id}</h1><footer>&copy;2024 Wetube -  All rights reserved</footer><body></html>`
+	);
 };
 export const edit = (req, res) => {
 	console.log(req.params);
---
+++ study/Templates.md
@@ -0,0 +1,4 @@
+# 5. Templates
+
+- 우리의 HTML은 생각보다 꽤 크다 -> `res.send()`로만 보내주기 힘들다
+  => Pug will save us
