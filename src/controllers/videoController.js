b471d45c622b4723584c2e3b132f4fcf5c366549
Eastyn <eastynwork@gmail.com>
2024-09-04 23:18:30 +0900

    #6.0 Array Database part One - learn how to use fake array database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,38 @@
+let videos = [
+	{
+		title: "First Video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 minutes ago",
+		views: 59,
+		id: 0,
+	},
+	{
+		title: "Video 2",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 minutes ago",
+		views: 59,
+		id: 1,
+	},
+	{
+		title: "Video 3",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 minutes ago",
+		views: 59,
+		id: 2,
+	},
+];
+
 export const trending = (req, res) => {
-	const videos = [
-		{
-			title: "First Video",
-			rating: 5,
-			comments: 2,
-			createdAt: "2 minutes ago",
-			views: 59,
-			id: 1,
-		},
-		{
-			title: "Video 2",
-			rating: 5,
-			comments: 2,
-			createdAt: "2 minutes ago",
-			views: 59,
-			id: 2,
-		},
-		{
-			title: "Video 3",
-			rating: 5,
-			comments: 2,
-			createdAt: "2 minutes ago",
-			views: 59,
-			id: 3,
-		},
-	];
 	res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch Video" });
+export const see = (req, res) => {
+	const { id } = req.params;
+	const video = videos[id];
+	res.render("watch", { pageTitle: `Watch Video ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload Video");
---
+++ study/MongoDB_and_Mongoose.md
@@ -0,0 +1,10 @@
+# 6 Mongo DB and Mongoose
+
+## 6.0-1 Array Database
+
+- How to send data to DB
+
+- we can use backtik with attributes!
+  ```
+  a(href=`/videos/${info.id}`)=info.title
+  ```
