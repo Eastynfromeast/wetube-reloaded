13422d58f72fa17126922a274b94a9241bc5cacc
Eastyn <eastynwork@gmail.com>
2024-09-11 18:34:19 +0900

    #7.0 Create Account part one - set up to let user create an account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,10 @@
-export const join = (req, res) => res.send("Join");
+export const getJoin = async (req, res) => {
+	return res.render("join", { pageTitle: "Join Us!" });
+};
+export const postJoin = async (req, res) => {
+	console.log(req.body);
+	res.end();
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
---
+++ study/User_Authentication.md
@@ -0,0 +1,7 @@
+# 7 User Authentication
+
+## 7.0-2 Create Account
+
+- VSCode 멀티 커서 방법
+  - option + 클릭
+  - Option + command + 방향키 위/ 아래
